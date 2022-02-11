a831d3e94ee55581d3c1218023d065a66d3f97f8
matteopelusi7 <pelusimatteo02@gmail.com>
2022-02-11 16:58:30 +0100

    Aggiunte ultime modifiche

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const title = [
     'Gran Bretagna',
     'Germania',
     'Paradise'
-]
+];
 
 const text = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
@@ -20,12 +20,13 @@ const text = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-]
+];
 
-let currentIndex = 0
+let currentIndex = 0;
 
 const slideWrapper = document.querySelector('.slide-wrapper');
 const imgSlide = document.querySelector('.img-slide');
+const slideText = document.querySelector('.slide-text');
 
 for (let i = 0; i < items.length; i++) {
 
@@ -45,7 +46,98 @@ for (let i = 0; i < items.length; i++) {
 
     imgSlide.innerHTML += itemImage;
 
+    const titleSlide = ` 
+            <h2 class="title-slider">${title[i]}</h2> `;
+
+    slideText.innerHTML += titleSlide;
+
+    const desSlide = `
+        <p class="description-slide">${text[i]}`;
+
+    slideText.innerHTML += desSlide;
+
 }
 
 const image = [...document.getElementsByClassName('item')];
 image[currentIndex].classList.add('active');
+
+const imageSlider = [...document.getElementsByClassName('item-slide')];
+imageSlider[currentIndex].classList.add('active');
+
+const titleSlider = [...document.getElementsByClassName('title-slider')];
+titleSlider[currentIndex].classList.add('active');
+
+const descripionSlider = [...document.getElementsByClassName('description-slide')];
+descripionSlider[currentIndex].classList.add('active');
+
+const buttonDown = document.querySelector('.fas.fa-chevron-down');
+
+buttonDown.addEventListener('click', function() {
+    
+    if ( currentIndex < image.length - 1 ) {
+
+        image[currentIndex].classList.remove('active');
+        imageSlider[currentIndex].classList.remove('active');
+        titleSlider[currentIndex].classList.remove('active');
+        descripionSlider[currentIndex].classList.remove('active');
+
+        currentIndex++;
+    
+        image[currentIndex].classList.add('active');
+        imageSlider[currentIndex].classList.add('active');
+        titleSlider[currentIndex].classList.add('active');
+        descripionSlider[currentIndex].classList.add('active');
+
+    } else {
+
+        image[currentIndex].classList.remove('active');
+        imageSlider[currentIndex].classList.remove('active');
+        titleSlider[currentIndex].classList.remove('active');
+        descripionSlider[currentIndex].classList.remove('active');
+
+        currentIndex = 0;
+    
+        image[currentIndex].classList.add('active');
+        imageSlider[currentIndex].classList.add('active');
+        titleSlider[currentIndex].classList.add('active');
+        descripionSlider[currentIndex].classList.add('active');
+
+    }
+
+})
+
+const buttonUp = document.querySelector('.fas.fa-chevron-up');
+
+buttonUp.addEventListener('click', function() {
+    
+    if ( currentIndex > 0 ) {
+
+        image[currentIndex].classList.remove('active');
+        imageSlider[currentIndex].classList.remove('active');
+        titleSlider[currentIndex].classList.remove('active');
+        descripionSlider[currentIndex].classList.remove('active');
+    
+        currentIndex--;
+    
+        image[currentIndex].classList.add('active');
+        imageSlider[currentIndex].classList.add('active');
+        titleSlider[currentIndex].classList.add('active');
+        descripionSlider[currentIndex].classList.add('active');
+
+    } else {
+
+        image[currentIndex].classList.remove('active');
+        imageSlider[currentIndex].classList.remove('active');
+        titleSlider[currentIndex].classList.remove('active');
+        descripionSlider[currentIndex].classList.remove('active');
+    
+        currentIndex = items.length - 1;
+    
+        image[currentIndex].classList.add('active');
+        imageSlider[currentIndex].classList.add('active');
+        titleSlider[currentIndex].classList.add('active');
+        descripionSlider[currentIndex].classList.add('active');
+
+    }
+
+})
