cb40d845a0ec2e4e0be0f5f98a322bf721c7b4cf
matteopelusi7 <pelusimatteo02@gmail.com>
2022-02-11 15:48:48 +0100

    Avanzamento js

--- app.js
+++ app.js
@@ -21,3 +21,31 @@ const text = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ]
+
+let currentIndex = 0
+
+const slideWrapper = document.querySelector('.slide-wrapper');
+const imgSlide = document.querySelector('.img-slide');
+
+for (let i = 0; i < items.length; i++) {
+
+    const element = items[i];
+
+    const item = `
+        <div class="item">
+            <img src="${items[i]}">
+        </div> `;
+
+    slideWrapper.innerHTML += item;
+
+    const itemImage = `
+        <div class="item-slide">
+            <img src="${items[i]}">
+        </div> `;
+
+    imgSlide.innerHTML += itemImage;
+
+}
+
+const image = [...document.getElementsByClassName('item')];
+image[currentIndex].classList.add('active');
